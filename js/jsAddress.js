4ce4f4d1ace0721e781e5e513b2bc900b4983f4b
tiaversa <75630776+tiaversa@users.noreply.github.com>
2021-10-26 06:32:11 -0600

    update on the address menu

--- js/jsAddress.js
+++ js/jsAddress.js
@@ -6,3 +6,19 @@ function takeShowOff(){
     document.getElementsByClassName("card").classList.remove('show');
 
 }
+
+
+//add active class in selected tab
+const list = document.querySelectorAll('.list');
+const cardList = document.querySelectorAll('.card');
+let dicList = {
+    0: 'dahlem',
+    1:'glienicke',
+    2:'lankwitz',
+}
+function activeLink(){
+    list.forEach((item) => 
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) => item.addEventListener('click', activeLink));
